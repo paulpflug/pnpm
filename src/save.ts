078896aafd9cfa21d2dbb5475e43d31d8a30b539
zkochan <zoltan.kochan@gmail.com>
2017-06-10 22:22:02 +0300

    fix: save packages to the correct properties
    Close #727

--- src/save.ts
+++ src/save.ts
@@ -1,6 +1,6 @@
 import loadJsonFile = require('load-json-file')
 import writePkg = require('write-pkg')
-import {DependenciesType} from './getSaveType'
+import {DependenciesType, dependenciesTypes} from './getSaveType'
 import {Package} from './types'
 import {PackageSpec} from './resolve'
 
@@ -10,13 +10,27 @@ export default async function save (
     name: string,
     saveSpec: string,
   })[],
-  saveType: DependenciesType
+  saveType?: DependenciesType
 ): Promise<Package> {
   // Read the latest version of package.json to avoid accidental overwriting
   const packageJson = await loadJsonFile(pkgJsonPath)
-  packageJson[saveType] = packageJson[saveType] || {}
+  if (saveType) {
+    packageJson[saveType] = packageJson[saveType] || {}
+  }
   packageSpecs.forEach(dependency => {
-    packageJson[saveType][dependency.name] = dependency.saveSpec
+    if (saveType) {
+      packageJson[saveType][dependency.name] = dependency.saveSpec
+      dependenciesTypes.filter(deptype => deptype !== saveType).forEach(deptype => {
+        if (packageJson[deptype]) {
+          delete packageJson[deptype][dependency.name]
+        }
+      })
+      return
+    }
+    const usedDepType: DependenciesType = dependenciesTypes
+      .find(deptype => packageJson[deptype] && packageJson[deptype][dependency.name]) || 'dependencies'
+    packageJson[usedDepType] = packageJson[usedDepType] || {}
+    packageJson[usedDepType][dependency.name] = dependency.saveSpec
   })
 
   await writePkg(pkgJsonPath, packageJson)
